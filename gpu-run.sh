1de765bd2233a680c3aa29800c769910e80cd851
wn1980 <w.n1980@outlook.com>
2019-11-01 07:30:45 +0700

    add run script

---
+++ gpu-run.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+p1080=1920x1080
+p720=1280x720
+p169=1600x900
+
+if [ $(uname -m) == 'x86_64' ] 
+then
+	tag=
+elif [ $(uname -m) == 'aarch64' ] 
+then 
+	tag=:rpi
+else
+	echo 'not matched platform!'
+	exit 0
+fi
+
+NAME=webots
+
+docker rm -f $NAME
+
+docker system prune -f
+
+docker run -it --rm --name $NAME --gpus all \
+	--network host \
+	--privileged \
+	-v /dev:/dev \
+	-v /run/systemd:/run/systemd \
+	-v /etc/localtime:/etc/localtime:ro \
+	-v /tmp/.X11-unix:/tmp/.X11-unix:rw \
+	-e VNC_PASSWORD=vnc123 \
+	-e VNC_RESOLUTION=$p1080 \
+	wn1980/docker-webots${tag} bash
